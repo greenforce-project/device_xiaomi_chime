f63a82d82c76bffd42447c923a22c27ef1256fcd
Lisa Liu <liulisa@google.com>
2020-07-15 19:02:07 +0800

    chime: Enable LZ4 compression for ramdisks
     * This shortens the time to unpack initramfs.
    
    Change-Id: Id23daba43ae7a6003cbde29480d2f433f344df73
    Signed-off-by: fadlyas07 <mhmmdfdlyas@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/chime
 TARGET_KERNEL_CONFIG := vendor/chime_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc
 
 # Platform
